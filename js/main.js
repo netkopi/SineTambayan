f09edf7a19ebb48db8261c0e083d0bba72da7b00
aguilar2005 <148366555+aguilar2005@users.noreply.github.com>
2024-01-18 19:53:46 +0800

    updated

--- js/main.js
+++ js/main.js
@@ -1,51 +1,43 @@
-const heroImages = [
-    {
-        source: 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-        type: 'Movie',
-        id: ''
-    },
-    {
-        source: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SX300.jpg',
-        type: 'Comedy',
-        id: ''
-    },
-    {
-        source: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg',
-        type: 'Movie',
-        id: ''
-    },
-    {
-        source: '',
-        type: '',
-        id: ''
-    },
-]
-
-
-
 // Hero Images //
 const hero = document.getElementById('hero');
+const textDiv = document.getElementById('type-container');
 const dotsDiv = document.getElementById('dots');
+
+
+
+
+
+
 
 heroImages.forEach(img => {
     const dot = document.createElement('div');
     dot.className = 'dot';
     
+    
     dotsDiv.appendChild(dot);
 });
 
 
+
+
+
 const dots = document.querySelectorAll('.dot');
-
-
-
 let index = 0;
 setInterval(() => {
-    const imgElem = document.createElement('img');
-    imgElem.style.position = 'absolute';
-    imgElem.style.animationName = 'fadein';
+    textDiv.innerHTML = '';
 
+
+
+    const imgElem = document.createElement('img');
+    const type = document.createElement('h1');
+
+
+    
+    type.textContent = heroImages[index].type;
+    type.style.color = heroImages[index].color;
     imgElem.src = heroImages[index].source;
+
+
 
 
     dots.forEach((dot) => {
@@ -54,18 +46,23 @@ setInterval(() => {
     dots[index].style.width = '2rem';
 
 
+
     index += 1;
     if(index > heroImages.length - 1){
-        imgElem.src = 'https://m.media-amazon.com/images/M/MV5BODY4OWM5M2UtM2Y1Yi00YjAyLTlhMDktMDkzZjFmMjI5MmI5XkEyXkFqcGdeQXVyMTA1NjQyNjkw._V1_SX300.jpg';
+        /* imgElem.src = 'https://m.media-amazon.com/images/M/MV5BODY4OWM5M2UtM2Y1Yi00YjAyLTlhMDktMDkzZjFmMjI5MmI5XkEyXkFqcGdeQXVyMTA1NjQyNjkw._V1_SX300.jpg'; */
         index = 0;
     }
+
+    textDiv.appendChild(type);
     hero.appendChild(imgElem);
-}, 2500);
+}, 3000);
 
-dots.forEach((dot, i) => {
+setTimeout(() => {
+    dots.forEach((dot, i) => {
+        dot.addEventListener('click', () => {
+            index = i;
+        });
     
-    dot.addEventListener('click', () => {
-        index = i;
     });
+}, 0)
 
-});
---
+++ src/utils/data-hero.js
@@ -0,0 +1,17 @@
+const heroImages = [
+    {
+        source: 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
+        type: 'Action',
+        id: '',
+    },
+    {
+        source: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SX300.jpg',
+        type: 'Fantasy',
+        id: '',
+    },
+    {
+        source: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg',
+        type: 'Comedy',
+        id: '',
+    },
+]
