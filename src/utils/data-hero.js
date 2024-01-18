595f7ab7c8a526832c34998e49d813e3f9b24836
aguilar2005 <148366555+aguilar2005@users.noreply.github.com>
2024-01-18 22:06:21 +0800

    updated

--- src/utils/data-hero.js
+++ src/utils/data-hero.js
@@ -1,17 +1,22 @@
 const heroImages = [
     {
         source: 'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-        type: 'Action',
+        type: 'Action Movies',
         id: '',
     },
     {
-        source: 'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SX300.jpg',
-        type: 'Fantasy',
+        source: 'https://m.media-amazon.com/images/M/MV5BZDRkZGUxZmEtZDdiZC00NGJmLWFiODAtN2RiMzY3ODhjNTNmXkEyXkFqcGdeQXVyMTg5MDEyNw@@._V1_SX300.jpg',
+        type: 'Western Movies',
         id: '',
     },
     {
-        source: 'https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg',
-        type: 'Comedy',
+        source: 'https://m.media-amazon.com/images/M/MV5BMDFiYzY5MDUtYjk4MS00NWJiLWE1NzMtMGUyOTUxYzk3NTg0XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_SX300.jpg',
+        type: 'Dramas',
+        id: '',
+    },
+    {
+        source: 'https://m.media-amazon.com/images/M/MV5BM2YwYTkwNjItNGQzNy00MWE1LWE1M2ItOTMzOGI1OWQyYjA0XkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_SX300.jpg',
+        type: 'Animes',
         id: '',
     },
 ]
